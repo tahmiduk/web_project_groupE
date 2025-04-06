d4dc48d6e0a46d7aff06e94bd39c25745210434e
tahmiduk <tahmid-ma@ulster.ac.uk>
2025-04-06 14:12:01 +0100

    Defined Product lists

---
+++ Solehub/js/scripts.js
@@ -0,0 +1,13 @@
+const products = [
+  { id: 1, name: "Cloud Runner", price: 129.99, image: "images/shoe1.jpg" },
+  { id: 2, name: "Urban Trekker", price: 159.99, image: "images/shoe2.jpg" },
+  { id: 3, name: "Retro Classic", price: 89.99, image: "images/shoe3.jpg" }
+];
+
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+$(document).ready(function() {
+  if (window.location.pathname.includes('products.html')) renderProducts();
+  if (window.location.pathname.includes('cart.html')) renderCart();
+  updateCartCounter();
+});
