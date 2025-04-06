84a5ff2ab199f8cf158d0b331ef0c4db6e6b865c
tahmiduk <tahmid-ma@ulster.ac.uk>
2025-04-06 14:13:30 +0100

    Render product list on products.html

--- Solehub/js/scripts.js
+++ Solehub/js/scripts.js
@@ -11,3 +11,22 @@ $(document).ready(function() {
   if (window.location.pathname.includes('cart.html')) renderCart();
   updateCartCounter();
 });
+
+function renderProducts() {
+  const grid = $('.products-grid');
+  grid.empty();
+  products.forEach(product => {
+    grid.append(`
+      <div class="col">
+        <div class="card h-100">
+          <img src="${product.image}" class="card-img-top" alt="${product.name}">
+          <div class="card-body">
+            <h5 class="card-title">${product.name}</h5>
+            <p class="card-text">$${product.price}</p>
+            <button class="btn btn-primary" onclick="addToCart(${product.id})">Add to Cart</button>
+          </div>
+        </div>
+      </div>
+    `);
+  });
+}
